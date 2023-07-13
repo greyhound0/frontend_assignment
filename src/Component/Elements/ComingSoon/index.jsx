77c1a9c2cd7efeb9d6878c6b4438cf367cebc819
manas gupta <manasgupta0110@gmail.com>
2023-07-13 20:06:22 +0530

    profile added

---
+++ src/Component/Elements/ComingSoon/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComingSoon() {
+  return (
+    <h1 style={{ fontSize: "8  0px", color: "rgb(238,238,238)" }}>
+      Coming Soon
+    </h1>
+  );
+}
+
+export default ComingSoon;
